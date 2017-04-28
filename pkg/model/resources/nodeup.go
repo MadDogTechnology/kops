90dd9489251efaa0c221add302df7a017d11c6a6
Derek VerLee <dverlee@maddogtechnology.com>
2017-04-28 16:52:27 -0400

    Hack cloud-init

--- pkg/model/resources/nodeup.go
+++ pkg/model/resources/nodeup.go
@@ -40,25 +40,26 @@ NODEUP_HASH={{ NodeUpSourceHash }}
 
 {{ S3Env }}
 
-export http_proxy=http://proxy.dc.res0.local:3128
-export https_proxy=http://proxy.dc.res0.local:3128
-export ftp_proxy=http://proxy.dc.res0.local:3128
-export no_proxy=s3.amazonaws.com,dc.res0.local,kopsalpha0.dc.res0.net,localhost,169.254.169.254
+PROXY=proxy.dc.res0.local:3128
+NOPROXY=localhost,127.0.0.1,100.64.0.1,.local,.internal,.res0.net,s3.amazonaws.com
 
-cat >> /etc/default/docker << __ETC_DEFAULT_DOCKER 
+export http_proxy=http://${PROXY}
+export https_proxy=http://${PROXY}
+export ftp_proxy=http://${PROXY}
+export no_proxy=${NOPROXY}
 
-export http_proxy=http://proxy.dc.res0.local:3128
-export https_proxy=http://proxy.dc.res0.local:3128
-export ftp_proxy=http://proxy.dc.res0.local:3128
-export no_proxy=s3.amazonaws.com,dc.res0.local,kopsalpha0.dc.res0.net,localhost,169.254.169.254
+cat >> /etc/default/docker << __ETC_DEFAULT_DOCKER
+export http_proxy=http://${PROXY}
+export https_proxy=http://${PROXY}
+export ftp_proxy=http://${PROXY}
+export no_proxy=${NOPROXY}
 __ETC_DEFAULT_DOCKER
 
-echo 'DefaultEnvironment=http_proxy=proxy.dc.res0.local:3128 https_proxy=proxy.dc.res0.local:3128 ftp_proxy=proxy.dc.res0.local:3128 no_proxy=localhost,169.254.169.254,s3.amazonaws.com,dc.res0.local,kopsalpha0.dc.res0.net' >> /etc/systemd/system.conf
+echo DefaultEnvironment=http_proxy=${PROXY} https_proxy=${PROXY} ftp_proxy=${PROXY} no_proxy=${NOPROXY} >> /etc/systemd/system.conf
 systemctl daemon-reexec
 
-
 rm -f /etc/apt/apt.conf
-echo 'Acquire::http::Proxy "http://proxy.dc.res0.local:3128";' > /etc/apt/apt.conf.d/30proxy
+echo 'Acquire::http::Proxy "http://${PROXY}";' > /etc/apt/apt.conf.d/30proxy
 
 function ensure-install-dir() {
   INSTALL_DIR="/var/cache/kubernetes-install"
